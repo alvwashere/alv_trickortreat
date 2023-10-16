e44097d17cd659a5346b94e1376d50d130d1ffd6
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 14:13:52 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,13 @@
 Config = {}
 
-Config.Treats = {
+Config.Chance = 75 -- Chance of receiving a treat in %. Set to false, 0/100 or above to always get a treat, else a number between 1 and 100. 
+
+Config.Process = {
+  KnockAnimation = {dict = '', anim = ''},
+  Prop = '',
+}
+
+Config.Treats = { -- Table formatted: itemName, amount, label.
   [1] = {itemName = 'bread', amount = math.random(1, 3), label = 'Bread'},
   [2] = {itemName = 'water', amount = math.random(1, 3), label = 'Water'},
 }
@@ -17,3 +24,5 @@ Config.Trick = {
 Config.Locations = {
   vec4(0.0, 0.0, 0.0, 0.0),
 }
+
+Config.Debug = true
