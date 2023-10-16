19c7216846104a8a73e4db12e44ba0fbeb63fb14
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 13:51:51 +0100

    Update config.lua
    Create initial config

--- config.lua
+++ config.lua
@@ -1,1 +1,19 @@
+Config = {}
 
+Config.Treats = {
+  [1] = {itemName = 'bread', amount = math.random(1, 3), label = 'Bread'},
+  [2] = {itemName = 'water', amount = math.random(1, 3), label = 'Water'},
+}
+
+Config.Trick = {
+  Ped = {
+    Spawn = true,
+    Model = 's_m_y_clown_01',
+    Attack = true,
+    Weapon = 'WEAPON_HOMINGLAUNCHER'
+  }
+}
+
+Config.Locations = {
+  vec4(0.0, 0.0, 0.0, 0.0),
+}
