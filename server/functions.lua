86e0860936b1c9e2d4b01fa7d59ac87e2f6f144a
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 14:26:11 +0100

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -1,1 +1,12 @@
+function GivePlayerItem(source, name, amount, label)
+  if ESX then
+    local xPlayer = ESX.GetPlayerFromId(source)
 
+    xPlayer.addInventoryItem(name, amount)
+    return xPlayer.showNotification(locale('received', label, amount))
+  elseif QBCore then
+
+  elseif OX then
+
+  end
+end
