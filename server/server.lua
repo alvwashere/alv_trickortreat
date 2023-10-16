41ae867ab1a9143e1816037ea7bf07828f3d8965
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 14:27:45 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,9 @@
+lib.callback.register('alv_trickortreat:treat', function(source, cb)
+  local Reward = Config.Treats[math.random(0, #Config.Treats]
 
+  GivePlayerItem(source, Reward.itemName, Reward.amount, Reward.label)
+end)
+
+lib.callback.register('alv_trickortreat:trick', function(source, cb)
+
+end)
