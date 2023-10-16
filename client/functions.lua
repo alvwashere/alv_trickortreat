620d4bfd14d454efd94ca0997b755afedc91e40d
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 13:56:07 +0100

    Update functions.lua
    create trick or treat function to determine whether player receives a trick or treat

--- client/functions.lua
+++ client/functions.lua
@@ -1,1 +1,19 @@
+function DebugPrint(msg)
+  if Config.Debug then 
+    print(msg)
+  end
+end
 
+function TrickOrTreat()
+  local number = math.random(0, 100)
+
+  if Config.Chance and Config.Chance < 100 and > 0 then
+    if number > Config.Chance then
+      return 'trick'
+    else
+      return 'treat'
+    end
+  else
+    return 'treat'
+  end
+end
